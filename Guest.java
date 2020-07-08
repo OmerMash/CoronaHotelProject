497b3151ac13e9461ac3262b84b66e4190124b61
OmerMash <59287988+OmerMash@users.noreply.github.com>
2020-07-08 14:46:32 +0300

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -1,16 +1,18 @@
+import java.util.Scanner;
 
-public class Guest extends Input {
+public class Guest{
 	
 	//Variables
-    protected static int guestCount;
-    protected static int numOfGuestsToCheckIn;
-    protected String Name = "guest_" + guestCounter();
+    protected static int guestCount=0;
+    protected int numOfGuestsToCheckIn;
+    protected String Name;
     protected int Sex;
     protected int Age;
     protected int roomNumber;
-    protected int UID;
+    protected double UID;
     protected int Religious;
-    
+	public static Scanner input = new Scanner(System.in);	
+
     
     //Methods
     //Ctor
@@ -19,32 +21,55 @@ public class Guest extends Input {
     	guestCount++;
     }
 
+    /*public static Guest createGuest() {
+    	  	Guest guest = new Guest();	
+    	  	System.out.println("Please fill-up Guest Details for Check-in :");
+    	  	System.out.println();
+        	System.out.println();
+        	
+        	
+        	System.out.println("Guest Details:");
+        	System.out.println("Guest Name:");
+        	guest.Name = input.next();
+        	System.out.println("Guest ID: ");
+    		guest.UID = input.nextDouble();
+        	
+        	while(guest.Sex != 1 && guest.Sex !=2) {
+        		System.out.println("Guest Sex: 1-male, 2-female ");
+        		guest.Sex = input.nextInt();
+            	if(guest.Sex != 1 && guest.Sex !=2) {
+            		System.out.println("Please Choose 1 or 2 only.");
+            		System.out.println();
+            	}
+        	}  	
+        	while(guest.Age <= 0 || guest.Age > 120) {
+        		System.out.println("Guest Age:");
+        		guest.Age = input.nextInt();
+            	if(guest.Age <= 0 || guest.Age > 120) {
+            		System.out.println("Please enter valid age only.");
+            		System.out.println();
+            	}
+        	}
+        	while(guest.Religious != 1 && guest.Religious !=2) {
+        		System.out.println("Religious?:  1-Yes, 2-No ");
+        		guest.Religious = input.nextInt();
+            	if(guest.Religious != 1 && guest.Religious !=2) {
+            		System.out.println("Please Choose 1 or 2 only.");
+            		System.out.println();
+            	}
+        	}
+        	return guest;
+    	}
+*/
+        	
+        	
+    
+    
+    
     public static int guestCounter() {
         return guestCount;
-    }
+    }  
 
-    public void getNumOfGuests() {
-    	System.out.print("Please enter number of Guests for check-in: ");
-    	numOfGuestsToCheckIn = input.nextInt();
-    }
-    
-    public void createGuest() {
-    	Guest guest = new Guest();	
-    	for(int i = 1  ;  i <= numOfGuestsToCheckIn  ;  i++) {
-        	System.out.println();
-        	System.out.println("Please enter Guest "+i+" info:");
-        	System.out.println("Guest "+ i +" Name:");
-        	Name = input.next();
-        	System.out.println("Guest "+ i +" Sex: 1-male, 2-female ");
-        	Sex = input.nextInt();
-        	System.out.println("Guest "+ i +" Age: ");
-        	Age = input.nextInt();
-        	System.out.println("Guest "+ i +" ID: ");
-        	UID = input.nextInt();
-        	System.out.println("Guest "+ i +" Religious?:  1-Yes, 2-No ");
-        	Religious = input.nextInt();
-    	}
-    }
     
     public void printCheckInDetails() {
     	System.out.println();
