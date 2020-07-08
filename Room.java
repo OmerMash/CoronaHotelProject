a79d4a95a114c8536b808338ad40f1c5e58eb4b2
OmerMash <59287988+OmerMash@users.noreply.github.com>
2020-07-08 14:44:28 +0300

    Update Room.java

--- Room.java
+++ Room.java
@@ -5,26 +5,24 @@ public class Room{
 	      int[] RoomsNumbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,
 			51,52,53,54,55,56,57,58,59,60,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100};
 		  
-	      public enum roomType {
-		    ONE_BEDRM, TWO_BEDRM, THREE_BEDRM, PENTHOUSE
-		  }
+	      //String roomType[] {ONE_BEDRM, TWO_BEDRM, THREE_BEDRM, PENTHOUS}
 //		  public enum FloorNum {
 //			ONE_Recovering, TWO_SuspectedAsACoronaPatient, THREE_CoronaPatient , FOUR_ImportantPatients
 //		  }
 
+	      public Guest roomGuests[];
 		  private boolean available;
 		  private UserGuest occupant;
-		  private int roomNumber;
-		  private double price;
-		  private roomType type;
+		  public int roomNumber = 0;
+		  public String roomtype = null;
+		  private int roomCapacity;
 //		  private FloorNum FloorN;
 
   			//C`tor
-		  public Room(int roomNumber, double price, roomType type) {
-		    this.roomNumber = roomNumber;
-		    this.price = price;
-		    this.type = type;
-//		    this.FloorN= FloorN;
+		  public Room(int guestsNumber) {
+		    roomNumber ++;
+		    this.roomCapacity = guestsNumber;
+		    
 		  }
 
 		  public boolean isAvailable() {
@@ -39,11 +37,11 @@ public class Room{
 		    this.available = available;
 		  }
 
-		  public double getPrice() {
+		 /* public double getPrice() {
 		    return price;
-		  }
+		  }*/
 
-		  public void setPrice(double price) {
+		 /* public void setPrice(double price) {
 		    this.price = price;
 		  }
 
@@ -53,7 +51,7 @@ public class Room{
 
 		  public void setRoomType(roomType type) {
 		    this.type = type;
-		  }
+		  }*/
 		  public int getRoomNumber() {
 			    return roomNumber;
 		  }
