6801681b252ba13efb8c85f8a0bc3961cb9104e6
Arielle Mizrahi <65770037+ArielleMizrahi@users.noreply.github.com>
2020-07-10 00:38:26 +0300

    Add files via upload

--- test/LoginTest.java
+++ test/LoginTest.java
@@ -1,47 +1,42 @@
-//package com.CoronaHotel.test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.BeforEach;
-//
-//class LoginTest {
-//	Login loginControlle;
-//	
-//	public void SetUp() {
-//		Login loginController= new Login();
-//	}
-//    
-//    //@Disabled - test not working/ quieting 
-//    //RepeatedTest(5) - running 5 times 
-//    //BeforEach - THIS ONE WILL RUN BEFOR EACH TEST 
-//    //AfterEach - THIS ONE WILL RUN AFTER EACH TEST 
-//    //BeforAll- THIS ONE WILL RUN BEFOR ALL TEST 
-//    //AfterAll- THIS ONE WILL RUN AFTER ALL TEST 
-//	@Test
-//	public void loginFailedWhenUsernameIsEmpty() {
-//    	//LoginController loginController= new LoginController();
-//    	try{
-//    		loginController.Login(" ","123");
-//    		Assertions.fail("The Login success but should failed");
-//    	}catch (IllegalArgumentException e) {
-//    		Assertions.assertEquals("UserName or password must not be null", e.getMessage());
-//    	}
-//    }
-//    @Test
-//    	public void loginFailedWhenPasswordIsEmpty() {
-//        	//LoginController loginController= new LoginController();
-//        	try{
-//        		loginController.Login("123"," ");
-//        		Assertions.fail("The Login success but should failed");
-//        	}catch (IllegalArgumentException e) {
-//        		Assertions.assertEquals("UserName or password must not be null", e.getMessage());
-//        	}
-//        }
-//    	
-//    	
-//	
-//	}
-//
-//}
+package hit.co.il.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import hit.co.il.Model;
+
+class LoginTest {
+	Model loginController;
+
+	public void SetUp() {
+		loginController = new Model();
+	}
+
+	// @Disabled - test not working/ quieting
+	// RepeatedTest(5) - running 5 times
+	// BeforEach - THIS ONE WILL RUN BEFOR EACH TEST
+	// AfterEach - THIS ONE WILL RUN AFTER EACH TEST
+	// BeforAll- THIS ONE WILL RUN BEFOR ALL TEST
+	// AfterAll- THIS ONE WILL RUN AFTER ALL TEST
+	@Test
+	public void loginFailedWhenUsernameIsEmpty() {
+		boolean RetValue = loginController.StringFinder(" ,123.", loginController.file.getAbsolutePath());
+		Assertions.assertTrue(RetValue, "The Login passed but should fail");
+	}
+
+	@Test
+	public void loginFailedWhenPasswordIsEmpty() {
+		boolean RetValue = loginController.StringFinder("123, .", loginController.file.getAbsolutePath());
+		Assertions.assertTrue(RetValue, "The Login passed but should fail");
+	}
+	@Test
+	public void loginSucceededWhenUserExists() {
+		boolean RetValue = loginController.StringFinder("ori,1234.", loginController.file.getAbsolutePath());
+		Assertions.assertTrue(RetValue, "The Login Succeeded!");
+	}
+	@Test
+	public void loginFailedWhenUserNotExists() {
+		boolean RetValue = loginController.StringFinder("ori,123.", loginController.file.getAbsolutePath());
+		Assertions.assertTrue(RetValue, "The Login passed but should fail");
+	}
+}
